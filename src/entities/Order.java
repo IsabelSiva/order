1b788947d8daa69538ecf2a902585d22b80c694d
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2022-07-10 15:37:24 -0300

    chore: generate getters and setters on Order and OrderItem classes, and create methods to calculate the value of orders

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -11,4 +11,35 @@ public class Order {
     private OrderStatus status;
 
     private List<OrderItem> items = new ArrayList<>();
+
+    public Order(){}
+
+    public Order(Date moment, OrderStatus status){
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void addItem(OrderItem item){
+        items.add(item);
+    }
+
+    public void removeItem(OrderItem item){
+        items.remove(item);
+    }
 }
